4b1c03e1ca3e778fb0859f067477224d8256c460
Matt Husby <matthusby@gmail.com>
2015-01-03 02:00:34 -0600

    Chapter 3,4 Convert the area function to take a tuple as its only param.

--- lib/geom.ex
+++ lib/geom.ex
@@ -3,36 +3,40 @@ defmodule Geom do
   Some handy dandy math stuffs
 
   ## Examples
-  iex> Geom.area(:rectangle, 3, 4)
+  iex> Geom.area({:rectangle, 3, 4})
   12
 
-  iex> Geom.area(:triangle, 3, 5)
+  iex> Geom.area({:triangle, 3, 5})
   7.5
 
-  iex> Geom.area(:ellipse, 2, 4)
+  iex> Geom.area({:ellipse, 2, 4})
   25.132741228718345
 
-  iex> Geom.area(:rectangle, -3, 4)
+  iex> Geom.area({:rectangle, -3, 4})
   0
 
-  iex> Geom.area(:matt, 2, 4)
+  iex> Geom.area({:matt, 2, 4})
   0
 
   """
 
-  def area(:rectangle, x, y) when x >= 0 and y >= 0 do
+  def area({shape, x, y}) do
+    area(shape, x, y)
+  end
+
+  defp area(:rectangle, x, y) when x >= 0 and y >= 0 do
     x * y
   end
 
-  def area(:triangle, x, y) when x >= 0 and y >= 0 do
+  defp area(:triangle, x, y) when x >= 0 and y >= 0 do
     ( x * y ) / 2
   end
 
-  def area(:ellipse, x, y) when x >= 0 and y >= 0 do
+  defp area(:ellipse, x, y) when x >= 0 and y >= 0 do
     :math.pi * x * y
   end
 
-  def area(_, _, _) do
+  defp area(_, _, _) do
     0
   end
 end
