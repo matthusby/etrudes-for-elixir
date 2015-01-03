2099e83c545f709ea3f34dda210d09dca959d5ac
Matt Husby <matthusby@gmail.com>
2015-01-03 01:50:09 -0600

    Chapter 3,2 Add guards to the area functions and update the tests.

--- lib/geom.ex
+++ lib/geom.ex
@@ -12,17 +12,20 @@ defmodule Geom do
   iex> Geom.area(:ellipse, 2, 4)
   25.132741228718345
 
+  iex> Geom.area(:rectangle, -3, 4)
+  ** (FunctionClauseError) no function clause matching in Geom.area/3
+
   """
 
-  def area(:rectangle, x, y) do
+  def area(:rectangle, x, y) when x >= 0 and y >= 0 do
     x * y
   end
 
-  def area(:triangle, x, y) do
+  def area(:triangle, x, y) when x >= 0 and y >= 0 do
     ( x * y ) / 2
   end
 
-  def area(:ellipse, x, y) do
+  def area(:ellipse, x, y) when x >= 0 and y >= 0 do
     :math.pi * x * y
   end
 
