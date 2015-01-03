e5dcb7b83eb3993236f1fee3b3a19267fa281ced
Matt Husby <matthusby@gmail.com>
2015-01-03 01:43:04 -0600

    Chapter 3,1 Update the area function to allow for pattern matching for different types.

--- lib/geom.ex
+++ lib/geom.ex
@@ -3,15 +3,27 @@ defmodule Geom do
   Some handy dandy math stuffs
 
   ## Examples
-  iex> Geom.area 4, 3
+  iex> Geom.area(:rectangle, 3, 4)
   12
 
-  iex> Geom.area 12, 7
-  84
+  iex> Geom.area(:triangle, 3, 5)
+  7.5
+
+  iex> Geom.area(:ellipse, 2, 4)
+  25.132741228718345
 
   """
 
-  def area(x,y) do
+  def area(:rectangle, x, y) do
     x * y
   end
+
+  def area(:triangle, x, y) do
+    ( x * y ) / 2
+  end
+
+  def area(:ellipse, x, y) do
+    :math.pi * x * y
+  end
+
 end
